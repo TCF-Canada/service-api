f566c1540182eb78166fb76446e50d71c6c24eea
Syphax LAKHDARCHAOUCHE <81050260+syphaxlch@users.noreply.github.com>
2025-06-22 22:29:52 -0400

    Add Swagger support with minimal API

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+// Register services needed for Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1",
+        Description = "A simple example ASP.NET Core Web API"
+    });
+});
 
-app.MapGet("/", () => "Hello World!");
+// Add services to the container.
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();      // Serve Swagger JSON
+    app.UseSwaggerUI();    // Serve Swagger UI web page
+}
+app.MapGet("/test", () => "Hello World!");
 
 app.Run();
